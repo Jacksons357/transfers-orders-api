48e8820ad8981ab6a06667593118ef2a8d7ca844
Jackson <jacksyntax20@gmail.com>
2024-10-03 19:22:36 -0300

    updating migrate

--- src/routes/get-transfers.ts
+++ src/routes/get-transfers.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
-import { prisma } from "../lib/prisma";
+import prisma from "../lib/prisma";
 
 export async function getTransfers(app: FastifyInstance){
   app.withTypeProvider<ZodTypeProvider>().get('/', 
